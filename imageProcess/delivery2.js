fcd40702aeb2155014530ba9a8004cbf1de5a08f
vlad <uplight.ca@gmail.com>
2016-05-17 17:20:04 -0400

    size

--- imageProcess/delivery2.js
+++ imageProcess/delivery2.js
@@ -55,8 +55,10 @@ var FileCopyer = (function () {
                     if (dim.width + dim.height < 6000) {
                         _this.copyFile(_this.srcDir, _this.destDir, next);
                     }
-                    else
-                        _this.onErrorCopy(' wrong file size  ' + next + 'width+heihgt ' + dim.width + dim.height + ' where max 6000 ', next);
+                    else {
+                        _this.onErrorCopy(' wrong file size  ' + next + 'width+heihgt ' + (dim.width + dim.height) + ' where max 6000 ', next);
+                        _this.doNext();
+                    }
                     // this.copyFile(this.srcDir,this.destDir,next);
                 });
             }
